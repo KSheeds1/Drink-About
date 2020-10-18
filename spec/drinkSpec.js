2f223aae48bb2580411605e57a7d276f57e5116e
KSheeds1 <katiesheedy@hotmail.com>
2020-10-18 21:34:31 +0000

    Describe initial testing for 'What can I drink' in 'drinkSpecs.js' Test fails due to reference error: 'whatCanIDrink' is not defined.

---
+++ spec/drinkSpec.js
@@ -0,0 +1,7 @@
+describe("whatCanIDrink", function() {
+   describe("should check age", function() {
+       it ("should return number", function() {
+           expect(whatICanDrink).toBeDefined();
+       });
+   }); 
+});
